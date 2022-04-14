78898cfea019e0fdd9d8a19d2b2fc9212f6ddcfb
ecometto <ecometto@hotmail.comm>
2022-04-14 10:49:00 -0300

    componente contador con estilos finalizado

--- my-app/src/components/Contador.js
+++ my-app/src/components/Contador.js
@@ -1,38 +1,55 @@
 import React, { useState } from 'react';
 
+import './contador.css'
 
 
 const Contador = () => {
-    
+
+    const numberStyle = {
+        margin: "20px auto",
+        width: "100px",
+        background: "black",
+        color: "rgb(254,254,254)",
+        fontSize: "30px",
+        padding: "10px",
+
+        textAlign: "center",
+    }
     const [numero, setNumero] = useState(0);
     const [contador, setContador] = useState(0);
     const [valor, setValor] = useState(0);
 
-    const actualizaValor = ()=>{
+    const actualizaValor = () => {
         setValor(document.getElementById('input').value)
     }
-    const incrementar = ()=>{
-        setNumero(numero + valor)
+    const incrementar = () => {
+        setNumero(numero + parseInt(valor))
+        setContador(contador + 1)
     }
-    const decrementar = ()=>{
-        setNumero(numero - valor)
+    const decrementar = () => {
+        setNumero(numero - parseInt(valor))
+        setContador(contador - 1)
     }
 
 
     return (
         <div>
-            <div className='container d-flex justify-content-center m-5'>
-                <div className='d-flex justify-content-center p-5 flex-column'>
-                    Ingrese el multiplo que quiere incrementar / decrementar <br />
-                    <input onChange={actualizaValor} type="text" id='input'/>
-
-                    <br />
-              
-                    <p>La multiplicacion de {contador} x {valor} es: </p>
-                    <p>{numero}</p>
+            <div className='mainContainer '>
+                <div className='counterContainer'>
+                    <p className="fw-bold"> Ingrese el multiplo que quiere incrementar / decrementar <br /></p>
+                    <input 
+                    onChange={actualizaValor} 
+                    type="number" 
+                    id='input' 
+                    autocomplete="off"
+                    />
 
                     <button onClick={decrementar}>Decrementar</button>
                     <button onClick={incrementar}>Incrementar</button>
+                    <br />
+                    <p>La multiplicacion de  {valor} x {contador} es: </p>
+                    <p style={numberStyle}>{numero}</p>
+
                 </div>
 
             </div>
