23a6fd33aabfddf3b36860aab916eb0659c29ed7
ecometto@hotmail.com <ecometto@hotmail.com>
2022-06-16 11:20:04 -0300

    mejorando todo list

--- final/src/components/Tarea.js
+++ final/src/components/Tarea.js
@@ -1,18 +1,36 @@
 import React from 'react'
 
-export default function Tarea({ tarea, editar, eliminar }) {
+export default function Tarea({ tarea, editar, eliminar, estado }) {
 
 
+    if (estado === "pendiente") {
+        return (
 
-    return (
-        <div>
-            <li className="d-flex justify-content-between bg-light m-1">
-                <p>{tarea.id} - {tarea.titulo} - {tarea.finalizado} </p>
-                <div>
-                    <button onClick={(e) => editar(e, tarea.id)} className='btn btn-sm btn-warning m-1'>Editar</button>
-                    <button onClick={(e) => eliminar(e, tarea.id)} className='btn btn-sm btn-danger m-1'>Eliminar</button>
-                </div>
-            </li>
-        </div>
-    )
-}
+            <div>
+                <li className="d-flex justify-content-between bg-light m-1">
+                    <p>{tarea.id} - {tarea.titulo} - {!tarea.finalizado ? "pendiente" : "finalizada"} </p>
+                    <div>
+                        <button onClick={(e) => editar(e, tarea.id)} className='btn btn-sm btn-warning m-1'>Editar</button>
+                        <button onClick={(e) => eliminar(e, tarea.id)} className='btn btn-sm btn-danger m-1'>Eliminar</button>
+                    </div>
+                </li>
+            </div>
+        )
+    }
+    else {
+        return (
+
+            <div>
+                <li className="d-flex justify-content-between bg-light m-1">
+                    <p>{tarea.id} - {tarea.titulo} - {!tarea.finalizado ? "pendiente" : "finalizada"} </p>
+                    {/* <div>
+                        <button onClick={(e) => editar(e, tarea.id)} className='btn btn-sm btn-warning m-1'>Editar</button>
+                        <button onClick={(e) => eliminar(e, tarea.id)} className='btn btn-sm btn-danger m-1'>Eliminar</button>
+                    </div> */}
+                </li>
+            </div>
+        )
+    }
+
+}    
+
--- final/src/pages/Tareas.js
+++ final/src/pages/Tareas.js
@@ -6,16 +6,25 @@ import Tarea from '../components/Tarea';
 
 export default function Tareas() {
     const [listaTareas, setListaTareas] = useState([]);
+    const [pendientes, setPendientes] = useState([...listaTareas])
+    const [concretadas, setConcretadas] = useState([])
     const [titulo, setTitulo] = useState("");
     const [id, setId] = useState(1)
 
-useEffect(()=>{
-    // alert('This is a welcome messagge. Enjoy creating Task')
-}
+    useEffect(() => {
 
-,[])
+        // const filtrar = ()=>{
+            const pendientes = listaTareas.filter((task) => task.finalizado === false)
+            const concretadas = listaTareas.filter((task) => task.finalizado === true)
+            setPendientes(pendientes)
+            setConcretadas(concretadas)
+        // }
+    } , [listaTareas])
 
     // funciones 
+
+
+
     const agregar = () => {
         let nuevaTarea = {
             "id": id,
@@ -26,6 +35,7 @@ useEffect(()=>{
         setId(id + 1)
         document.getElementById('titulo').value = ""
         document.getElementById('titulo').focus()
+
     }
 
     const actualizarTitulo = (e) => {
@@ -36,19 +46,26 @@ useEffect(()=>{
 
         let confirmar = window.confirm("seguro que desea eliminar?");
         if (confirmar) {
-            let msg = document.getElementById('msg')
-            msg.innerHTML = "Elemento Eliminado.."
-            msg.classList.add('alert-danger')
-
-            setTimeout(() => {
-                msg.innerHTML = ""
-                msg.classList.remove('alert-danger')
-            }, 1500);
-
-            let temp = listaTareas.filter((tarea) => tarea.id !== id)
+            const temp = [...listaTareas]
+            temp.map(tarea=> tarea.id===id?tarea.finalizado = true:""     
+                )
+                console.log(temp)
             setListaTareas(temp)
+
+            // let msg = document.getElementById('msg')
+            // msg.innerHTML = "Elemento Eliminado.."
+            // msg.classList.add('alert-danger')
+
+            // setTimeout(() => {
+            //     msg.innerHTML = ""
+            //     msg.classList.remove('alert-danger')
+            // }, 1500);
+
+            // let temp = listaTareas.filter((tarea) => tarea.id !== id)
+            // setListaTareas(temp)
         }
     })
+
     const editar = (e, id) => {
         let titulo = listaTareas.find(tarea => tarea.id === id)
         let nuevoTitulo = prompt('ingrese el nuevo valor', titulo.titulo)
@@ -74,7 +91,7 @@ useEffect(()=>{
 
             <h1 className='text-center mt-4 mb-2'>ToDo LIST</h1>
             <div className='title-container'>
-                <p>Here you can see a classic ToDo list. You'll be able to create, edit and close tasks.</p>
+                <p>In this page you can see a classic ToDo list. You'll be able to create, edit and close tasks.</p>
                 <p><u><b>Used tools:</b></u> Components, UseState, Props, UseEffect, others</p>
             </div>
 
@@ -92,22 +109,43 @@ useEffect(()=>{
 
                 </div>
                 <p id='msg' style={{ height: "40px" }} className="p-2"></p>
-                <h5 className='text-center'><u>LISTA DE TAREAS</u></h5>
+                <h5 className='text-center'><u>LISTA DE TAREAS PENDIENTES</u></h5>
                 <div className='col-8 offset-md-2'>
-                    
+
                     {
-                        listaTareas.map((tarea) => {
+                    pendientes.length>0?
+                        pendientes.map((tarea) => {
                             return (
-
-                        < Tarea  key={tarea.id} //props: 
-                        tarea={tarea} 
-                        editar={editar} 
-                        eliminar={eliminar}
-                        />
-
-    
-                )
+                                < Tarea key={tarea.id} //props: 
+                                    tarea={tarea}
+                                    editar={editar}
+                                    eliminar={eliminar}
+                                    estado = "pendiente"
+                                />
+                            )
                         })
+                        : <p className='text-center alert-success'>No se encontraron tareas pendientes</p>
+
+                        }
+                </div>
+                <h5 className='text-center mt-4'><u>LISTA DE TAREAS CONCRETADAS</u></h5>
+                <div className='col-8 offset-md-2'>
+                    {
+                    concretadas.length>0?
+                        concretadas.map(tarea=>{
+                            return(
+                                
+                                < Tarea key={tarea.id} 
+                                //props: 
+                                tarea={tarea}
+                                editar={editar}
+                                eliminar={eliminar}
+                                estado = "finalizada"
+                            />
+                            )
+                        } )      
+                        : <p className='text-center'>no se encontraron tareas finalizada </p>
+
                     }
                 </div>
 
